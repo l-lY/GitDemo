6051037abcc03011b47f9ff03f1706455f250075
l-l <1610154799@qq.com>
2023-04-22 10:08:04 +0800

    implement:buttonAdd on clickListener

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gitdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  var number = 0
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = "$number"
+        binding.buttonAdd.setOnClickListener{binding.textView.text = "${++number}"}
+
     }
 }
